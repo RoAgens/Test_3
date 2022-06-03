d7c4f006abb6a57338c53dc28521479fe093205f
RoAgens <r.konyaev@gmail.com>
2022-06-03 11:02:30 +0300

    Add project files.

---
+++ LevelsCommand.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_3.ViewModels;
+
+namespace Test_3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class LevelsCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            ViewModelMainWindow viewModelMainWindow = new ViewModelMainWindow(doc);
+            viewModelMainWindow.MainWindow.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,65 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_3.ViewModelsBase;
+using Test_3.Views;
+
+namespace Test_3.ViewModels
+{
+    public class ViewModelMainWindow : NotifyProperty
+    {
+        private Document _doc;
+        private List<Level> _levels;
+
+        private MainWindow _mainWindow;
+
+        public MainWindow MainWindow
+        {
+            get
+            {
+                if (_mainWindow == null)
+                {
+                    _mainWindow = new MainWindow() { DataContext = this };
+                }
+                return _mainWindow;
+            }
+
+            set
+            {
+                _mainWindow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<Level> Levels
+        {
+            get
+            {
+                if (_levels == null)
+                {
+                    _levels = new List<Level>();
+                }
+                return _levels;
+            }
+
+            set
+            {
+                _levels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModelMainWindow(Document doc)
+        {
+            _doc = doc;
+
+            _levels = new FilteredElementCollector(doc)
+            .WhereElementIsNotElementType()
+            .OfCategory(BuiltInCategory.INVALID)
+            .OfClass(typeof(Level)).Cast<Level>().ToList();
+        }
+    }
+}
